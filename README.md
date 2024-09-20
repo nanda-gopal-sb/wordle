0a327bb400385740836cf20a5fafb16171b48554
Nandagopal <nandagopal.nsb@gmail.com>
2024-09-20 19:51:43 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,55 @@
+# Wordle Game
+
+Welcome to the Wordle Game project! This is a simple implementation of the popular word-guessing game, Wordle, using C and C++.
+
+## Table of Contents
+
+- [Introduction](#introduction)
+- [Features](#features)
+- [Installation](#installation)
+- [Usage](#usage)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Introduction
+
+Wordle is a word puzzle game where the player has six attempts to guess a five-letter word. After each guess, the game provides feedback indicating which letters are correct and in the correct position, which letters are correct but in the wrong position, and which letters are not in the word at all.
+
+## Features
+
+- Simple and intuitive command-line interface
+- Random word selection from a predefined list
+- Feedback on each guess to help you solve the puzzle
+- Written in C and C++ for performance and portability
+
+## Installation
+
+To compile and run the Wordle game, follow these steps:
+
+1. Clone the repository:
+    ```sh
+    git clone https://github.com/nanda-gopal-sb/wordle.git
+    cd wordle
+    ```
+
+2. Compile the code:
+    ```sh
+    g++ -o wordle main.cpp
+    ```
+
+3. Run the game:
+    ```sh
+    ./wordle
+    ```
+
+## Usage
+
+Once the game is running, follow the on-screen instructions to guess the word. Enter your guesses and receive feedback until you either guess the word correctly or run out of attempts.
+
+## Contributing
+
+Contributions are welcome! If you have any ideas, suggestions, or bug reports, please open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more details.
---
+++ wordle.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
+#include <string.h>
+#include <random>
+#include <chrono>
+#include <thread>
+#define red 91
+#define green 92
+#define yellow 93
+using namespace std;
+random_device rd;
+mt19937 gen(rd());
+class gameFunctions
+{
+public:
+    void printColoredChar(char ch, int color);
+    void welcomeMessage();
+    string getWordForToday();
+    int random();
+    bool isPresentInsideString(string word, char toCheck);
+    void display(string word, string currentWord);
+};
+int gameFunctions::random()
+{
+    uniform_int_distribution<> dist(1, 5746);
+    return dist(gen);
+}
+void gameFunctions::display(string guess, string currentWord)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (guess[i] == currentWord[i])
+        {
+            printColoredChar(guess[i], green);
+            continue;
+        }
+        if (isPresentInsideString(currentWord, guess[i]))
+        {
+            printColoredChar(guess[i], yellow);
+            continue;
+        }
+        printColoredChar(guess[i], red);
+    }
+    cout << "\n";
+}
+void gameFunctions::printColoredChar(char ch, int color)
+{
+    cout << "\033[1;" << color << "m" << ch << "\033[0m";
+}
+bool gameFunctions::isPresentInsideString(string word, char toCheck)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        if (word[i] == toCheck)
+        {
+            return true;
+        }
+    }
+    return false;
+}
+void gameFunctions::welcomeMessage()
+{
+    cout << "Welcome to Wordle! C++ Style" << "\n";
+    cout << "Guess the Word in 6 tries." << "\n";
+    cout << "Each guess must be a valid 5-letter word." << "\n";
+    cout << "The color of the letters will change to show how close your guess was to the word." << "\n";
+    printColoredChar('W', green);
+    cout << "OODY" << "\n";
+    cout << "Here the letter W is in word and in the right place" << "\n";
+    cout << "L";
+    printColoredChar('I', yellow);
+    cout << "GHT" << "\n";
+    cout << "Here the letter I is in the word but in the wrong place" << "\n";
+    cout << "ROG";
+    printColoredChar('U', red);
+    cout << "E" << "\n";
+    cout << "Here the letter U is not in the word" << "\n";
+}
+string gameFunctions::getWordForToday()
+{
+    ifstream file("fiveLetterWords.dat");
+    string word;
+    int count = 0;
+    int rand = random();
+    while (file >> word)
+    {
+        count++;
+        if (count == rand)
+        {
+            break;
+        }
+    }
+    file.close();
+    return word;
+}
+int main()
+{
+    gameFunctions *game = new gameFunctions();
+    string guessArr[6];
+    game->welcomeMessage();
+    string currentWord = game->getWordForToday();
+    int numberOfTries = 0;
+    string guess;
+    int commonIndex = 0;
+    while (numberOfTries < 7)
+    {
+        if (numberOfTries == 6)
+        {
+            for (int i = 0; i < 6; i++)
+            {
+                game->display(guessArr[i], currentWord);
+            }
+            cout << "Sadly you lose the game..." << "\n";
+            cout << "The word was - " << currentWord << "\n";
+            break;
+        }
+        if (numberOfTries > 0)
+            system("cls");
+        for (int i = 0; i < numberOfTries; i++)
+        {
+            game->display(guessArr[i], currentWord);
+        }
+        cout << "Your Guess: ";
+        cin >> guess;
+        cout << "\n";
+        if (guess.length() != 5)
+        {
+            cout << "The entered word must be of 5 letters" << "\n";
+            this_thread::sleep_for(chrono::seconds(3));
+            continue;
+        }
+        else
+        {
+            if (currentWord == guess)
+            {
+                for (int i = 0; i < 5; i++)
+                {
+                    game->printColoredChar(guess[i], green);
+                }
+                cout << " is right" << "\n";
+                break;
+            }
+            else
+            {
+                guessArr[numberOfTries] = guess;
+            }
+        }
+        numberOfTries++;
+    }
+    delete game;
+}
